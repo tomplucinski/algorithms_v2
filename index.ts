03e4fe59be37cea0f0b4f0447edffc195b93072c
Tom Plucinski <tapluci1@gmail.com>
2022-07-27 07:58:54 -0700

    BubbleSort

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { sumTwoNumbers } from './arrays/sumTwoNumbers'
 import { bubbleSort } from './arrays/sorting/bubbleSort'
 
 const sumTwoNumbersResult = sumTwoNumbers([3, 5, -4, 8, 11, 1, -1, 6], 10)
-console.log('SumTwoNumbers', sumTwoNumbersResult)
+console.log('SumTwoNumbers:', sumTwoNumbersResult)
 
 const bubbleSortResult = bubbleSort([8, 5, 2, 9, 5, 6, 3])
-console.log('BubbleSort', bubbleSortResult)
+console.log('BubbleSort:', bubbleSortResult)
