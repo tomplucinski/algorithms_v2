ec815b1112973726a127b1ebfe7d2d59d3a26d05
Tom Plucinski <tapluci1@gmail.com>
2022-08-24 21:53:36 -0700

    Remove duplicates in linked list

---
+++ linkedLists/removeDuplicates.ts
@@ -0,0 +1,10 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList: LinkedList) {
+  return null;
+}
