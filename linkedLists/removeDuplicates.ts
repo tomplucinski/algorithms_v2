dae3f10e6592472988797eadd011899d74b69ba4
Tom Plucinski <tapluci1@gmail.com>
2022-08-27 11:16:23 -0700

    Update linked list solution

--- linkedLists/removeDuplicates.ts
+++ linkedLists/removeDuplicates.ts
@@ -1,14 +1,27 @@
 class LinkedList {
   constructor(value) {
-    this.value = value;
-    this.next = null;
+    this.value = value
+    this.next = null
   }
 }
 
-function removeDuplicatesFromLinkedList(linkedList: LinkedList[]) {
-  if (linkedList.next) {
-    const nextNode = removeDuplicatesFromLinkedList(linkedList.next);
-
-    return nextNode;
+function removeDuplicatesFromLinkedList(linkedList: any) {
+  const map = {}
+  let currentNode = linkedList
+  let parentNode = null
+  while (true) {
+    if (currentNode.next) {
+      if (!map[currentNode.value]) {
+        map[currentNode.value] = currentNode.value
+      } else {
+        parentNode.next = currentNode.next.value
+        currentNode.next = null
+      }
+      parentNode = currentNode
+      currentNode = currentNode.next
+    }
+    break
   }
+
+  return linkedList
 }
