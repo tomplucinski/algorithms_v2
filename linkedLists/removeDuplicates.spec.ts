89b235747cc88b1fc6912dde8d86942d49ba27f0
Tom Plucinski <tapluci1@gmail.com>
2022-08-29 21:27:25 -0700

    Instantiate tests for removeDuplicates

---
+++ linkedLists/removeDuplicates.spec.ts
@@ -0,0 +1,8 @@
+import { removeDuplicatesFromLinkedList } from './removeDuplicates'
+import { SingleLinkedList } from '../types'
+
+describe('LinkedList', () => {
+  it('Test Case #1', () => {
+    const input = new SingleLinkedList(1)
+  })
+})
